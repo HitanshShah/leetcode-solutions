af68e9d3d2031c365990ea19dda6a0fe1f1f2dde
Hitansh Shah <31037909+HitanshShah@users.noreply.github.com>
2024-04-18 22:52:50 -0700

    Time: 1 ms (93.22%) | Memory: 45.4 MB (77.92%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,20 +1,34 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        // int ans = 0;
+        // int len = prices.length;
+        // if(len == 1) return ans;
+        // int p1 = 0, p2 = 1;
+        // while(p2<len){
+        //     if(prices[p1] >= prices[p2]){
+        //         p1 = p2;
+        //         p2++;
+        //     }else{
+        //         while(p2+1 < len && prices[p2] < prices[p2+1]){
+        //             p2++;
+        //         }
+        //         ans += prices[p2] - prices[p1];
+        //         p1=p2;
+        //         p2++;
+        //     }
+        // }
+        // return ans;
+
         int ans = 0;
         int len = prices.length;
         if(len == 1) return ans;
-        int p1 = 0, p2 = 1;
-        while(p2<len){
-            if(prices[p1] >= prices[p2]){
-                p1 = p2;
-                p2++;
+        int ptr = 0;
+        for(int i=1; i<len; i++){
+            if(prices[ptr] > prices[i]){
+                ptr = i;
             }else{
-                while(p2+1 < len && prices[p2] < prices[p2+1]){
-                    p2++;
-                }
-                ans += prices[p2] - prices[p1];
-                p1=p2;
-                p2++;
+                ans += prices[i] - prices[ptr];
+                ptr = i;
             }
         }
         return ans;
