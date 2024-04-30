35c50606dc89c5547ae3ba5cc905ff9cc5ab3d27
Hitansh Shah <31037909+HitanshShah@users.noreply.github.com>
2024-04-30 13:59:25 -0700

    Time: 22 ms (47.58%) | Memory: 50.5 MB (72.80%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -8,7 +8,7 @@ class Solution {
                 if(text1.charAt(i-1) == text2.charAt(j-1)){
                     dp[i][j] = dp[i-1][j-1] + 1;
                 }else{
-                    dp[i][j] = dp[i-1][j] > dp[i][j-1] ? dp[i-1][j] : dp[i][j-1];
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
