ae263e7596eeb997d7de4959aef58f780b716f3a
Hitansh Shah <31037909+HitanshShah@users.noreply.github.com>
2024-05-19 17:15:18 -0700

    Time: 1 ms (83.74%) | Memory: 42.2 MB (28.30%) - LeetSync

--- 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.java
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.java
@@ -19,18 +19,6 @@ class Solution {
         q.offer(root);
         boolean flag = false;
         while(!q.isEmpty()){
-            // TreeNode curr = q.poll();
-            // System.out.println(curr.val);
-            // if(curr.left==null && curr.right==null){
-
-            // }else{
-            //     if(curr.left!=null) q.add(curr.left);
-            //     else counter++;
-            //     if(counter==0 && curr.right!=null) q.add(curr.right);
-            //     else if(counter == 0 && curr.right==null) counter++;
-            //     else return false;
-            // }
-            // if(counter>1) return false;
             TreeNode curr = q.poll();
             if(curr == null){
                 flag = true;
@@ -42,13 +30,4 @@ class Solution {
         }
         return true;
     }
-    // public boolean helper(TreeNode root){
-    //     if(root.right != null && root.left == null) return false;
-    //     if(root.left!=null){
-    //         if(root.right!=null) return (helper(root.left) && helper(root.right));
-    //         else return helper(root.left);
-    //     }else{
-    //         return true;
-    //     }
-    // }
 }
