b882efc5c453eccc1af7b04ac710934d137b8c12
Hitansh Shah <31037909+HitanshShah@users.noreply.github.com>
2024-05-22 00:05:20 -0700

    Time: 52 ms (73.25%) | Memory: 20.4 MB (46.36%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,11 +7,33 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if p.val == root.val or q.val == root.val:
-            return root
-        elif (p.val < root.val and q.val > root.val) or (p.val > root.val and q.val < root.val):
-            return root
-        elif p.val < root.val and q.val < root.val:
-            return self.lowestCommonAncestor(root.left, p, q)
-        else:
-            return self.lowestCommonAncestor(root.right, p, q)
+        # if p.val == root.val or q.val == root.val:
+        #     return root
+        # elif (p.val < root.val and q.val > root.val) or (p.val > root.val and q.val < root.val):
+        #     return root
+        # elif p.val < root.val and q.val < root.val:
+        #     return self.lowestCommonAncestor(root.left, p, q)
+        # else:
+        #     return self.lowestCommonAncestor(root.right, p, q)
+        foundp = False
+        foundq = False
+        root_p, root_q = root, root
+        ancestor = root
+        while (root_p and not foundp) and (root_q and not foundq):
+            if not foundp:
+                if p.val == root_p.val:
+                    foundp = True
+                elif p.val < root_p.val:
+                    root_p = root_p.left
+                else:
+                    root_p = root_p.right
+            if not foundq:
+                if q.val == root_q.val:
+                    foundq = True
+                elif q.val < root_q.val:
+                    root_q = root_q.left
+                else:
+                    root_q = root_q.right
+            if root_p.val == root_q.val:
+                ancestor = root_p
+        return ancestor
