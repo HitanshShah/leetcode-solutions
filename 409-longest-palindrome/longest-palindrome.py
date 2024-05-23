b8e75319586d6f478409ec6f73042425e0f34aed
Hitansh Shah <31037909+HitanshShah@users.noreply.github.com>
2024-05-22 20:03:04 -0700

    Time: 40 ms (33.32%) | Memory: 16.6 MB (86.91%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -1,6 +1,6 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        n = len(s)
+        n = 0
         counts = {}
         for c in s:
             counts[c] = counts.get(c, 0)+1
@@ -9,6 +9,10 @@ class Solution:
             if counts[i]%2 == 1:
                 if not found_one:
                     found_one = True
-                else:
-                    n-=1
-        return n
+                n+=counts[i]-1
+            else:
+                n+=counts[i]
+        if found_one:
+            return n+1
+        else:
+            return n
