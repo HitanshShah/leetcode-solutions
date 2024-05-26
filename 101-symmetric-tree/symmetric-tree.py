a52236ad592400b2d9aa766813de8f23199f0418
Hitansh Shah <31037909+HitanshShah@users.noreply.github.com>
2024-05-25 23:01:51 -0700

    Time: 41 ms (25.94%) | Memory: 16.4 MB (95.71%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.py
+++ 101-symmetric-tree/symmetric-tree.py
@@ -9,12 +9,10 @@ class Solution:
         return self.symmetric(root.left, root.right)
         
     def symmetric(self, root1, root2):
-        if not root1 and not root2:
-            return True
-        elif root1 and root2:
+        if not root1 or not root2:
+            return root1 == root2
+        else:
             if root1.val != root2.val:
                 return False
             else:
                 return self.symmetric(root1.left, root2.right) and self.symmetric(root1.right, root2.left)
-        else:
-            return False
