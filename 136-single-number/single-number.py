4b621360259c850e25783b4e954c5894464c9d7a
Hitansh Shah <31037909+HitanshShah@users.noreply.github.com>
2024-05-25 17:56:34 -0700

    Time: 110 ms (60.02%) | Memory: 19 MB (94.41%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,8 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        ans = nums[0]
-        for i in range(1,len(nums)):
+        ans = 0
+        for i in range(len(nums)):
             ans = ans^nums[i]
         return ans
