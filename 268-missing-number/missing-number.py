19bb7619bd2d3a2443fb3adbe7f9a7e8e2705662
Hitansh Shah <31037909+HitanshShah@users.noreply.github.com>
2024-05-25 23:17:32 -0700

    Time: 126 ms (19.59%) | Memory: 17.8 MB (29.95%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -17,12 +17,12 @@ class Solution:
         # return len(nums)
 
 
-        # n = len(nums)
-        # target = (n*(n+1))//2
-        # sums = 0
-        # for num in nums:
-        #     sums += num
-        # return target - sums
-
         n = len(nums)
-        return (n*(n+1)//2) - sum(nums)
+        target = (n*(n+1))//2
+        sums = 0
+        for num in nums:
+            sums += num
+        return target - sums
+
+        # n = len(nums)
+        # return (n*(n+1)//2) - sum(nums)
