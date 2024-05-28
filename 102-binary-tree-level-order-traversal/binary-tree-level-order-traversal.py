27290f90c14992f1a0c65fade1b4f9eedb667d62
Hitansh Shah <31037909+HitanshShah@users.noreply.github.com>
2024-05-27 19:54:52 -0700

    Time: 42 ms (43.44%) | Memory: 16.9 MB (99.91%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -6,23 +6,41 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        # ans = []
+        # if not root:
+        #     return ans
+        # q1 = deque()
+        # q2 = deque()
+        # q1.append(root)
+        # while q1:
+        #     curr_lev = []
+        #     while q1:
+        #         q2.append(q1.popleft())
+        #     while q2:
+        #         node = q2.popleft()
+        #         if node.left:
+        #             q1.append(node.left)
+        #         if node.right:
+        #             q1.append(node.right)
+        #         curr_lev.append(node.val)
+        #     # print(curr_lev)
+        #     ans.append(curr_lev)
+        # return ans
         ans = []
         if not root:
             return ans
         q1 = deque()
-        q2 = deque()
-        q1.append(root)
+        q1.append((root,0))
+        curr_lvl = -1
         while q1:
-            curr_lev = []
-            while q1:
-                q2.append(q1.popleft())
-            while q2:
-                node = q2.popleft()
-                if node.left:
-                    q1.append(node.left)
-                if node.right:
-                    q1.append(node.right)
-                curr_lev.append(node.val)
-            # print(curr_lev)
-            ans.append(curr_lev)
+            node, level = q1.popleft()
+            if curr_lvl != level:
+                ans.append([node.val])
+                curr_lvl = level
+            else:
+                ans[-1].append(node.val)
+            if node.left:
+                q1.append((node.left, level+1))
+            if node.right:
+                q1.append((node.right, level+1))
         return ans
