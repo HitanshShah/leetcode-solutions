5f85fb3a2c87a4b9d71b64d33bb72f96720e5e3e
Hitansh Shah <31037909+HitanshShah@users.noreply.github.com>
2024-05-29 16:53:14 -0700

    Time: 300 ms (5.78%) | Memory: 17.5 MB (7.99%) - LeetSync

---
+++ 78-subsets/subsets.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        curr = []
+        self.helper(nums, 0, ans, curr)
+        return ans
+
+    def helper(self, nums, idx, ans, curr):
+        print(curr, ans)
+        if curr not in ans:
+            ans.append(list(curr))
+        if idx == len(nums):
+            return
+        curr.append(nums[idx])
+        self.helper(nums, idx+1, ans, curr)
+        curr.pop(-1)
+        self.helper(nums,idx+1, ans, curr)
