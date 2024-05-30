e4603f8802ced73101729c4d819398e1d80eca07
Hitansh Shah <31037909+HitanshShah@users.noreply.github.com>
2024-05-30 16:02:11 -0700

    Time: 41 ms (78.94%) | Memory: 19.4 MB (91.87%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -6,14 +6,26 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        traversal = []
-        self.inOrder(root, traversal)
-        return traversal[k-1]
+    #     traversal = []
+    #     self.inOrder(root, traversal)
+    #     return traversal[k-1]
 
-    def inOrder(self, root, traversal):
-        if not root:
-            return
-        self.inOrder(root.left, traversal)
-        traversal.append(root.val)
-        self.inOrder(root.right, traversal)
-        return
+    # def inOrder(self, root, traversal):
+    #     if not root:
+    #         return
+    #     self.inOrder(root.left, traversal)
+    #     traversal.append(root.val)
+    #     self.inOrder(root.right, traversal)
+    #     return
+        traversal = []
+        curr = root
+        while curr or traversal:
+            while curr:
+                traversal.append(curr)
+                curr = curr.left
+            curr = traversal.pop()
+            k -= 1
+            if k == 0:
+                return curr.val
+            curr = curr.right
+        
