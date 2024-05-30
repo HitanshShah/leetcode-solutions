97fdc854f084d78c270be014a71201e2a7d3120b
Hitansh Shah <31037909+HitanshShah@users.noreply.github.com>
2024-05-29 17:34:21 -0700

    Time: 39 ms (38.12%) | Memory: 16.5 MB (88.84%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -6,17 +6,31 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+    #     ans = []
+    #     if not root:
+    #         return ans
+    #     self.helper(ans, root, 0)
+    #     return ans
+        
+    # def helper(self, ans, root, currLevel):
+    #     if currLevel == len(ans):
+    #         ans.append(root.val)
+    #     if root.right:
+    #         self.helper(ans, root.right, currLevel+1)
+    #     if root.left:
+    #         self.helper(ans, root.left, currLevel+1)
+    #     return
         ans = []
         if not root:
             return ans
-        self.helper(ans, root, 0)
+        queue = deque()
+        queue.append((root,0))
+        while queue:
+            node, lvl = queue.popleft()
+            if lvl == len(ans):
+                ans.append(node.val)
+            if node.right:
+                queue.append((node.right, lvl+1))
+            if node.left:
+                queue.append((node.left, lvl+1))
         return ans
-        
-    def helper(self, ans, root, currLevel):
-        if currLevel == len(ans):
-            ans.append(root.val)
-        if root.right:
-            self.helper(ans, root.right, currLevel+1)
-        if root.left:
-            self.helper(ans, root.left, currLevel+1)
-        return
